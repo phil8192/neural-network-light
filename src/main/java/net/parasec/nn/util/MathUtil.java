22f315206b2a88c9e593b84e0bd57b1147e9080f
phil8192 <phil@parasec.net>
2014-12-10 19:09:11 +0200

    fast sqrt() from tsp code. should maybe put this stuff in another package..

--- src/main/java/net/parasec/nn/util/MathUtil.java
+++ src/main/java/net/parasec/nn/util/MathUtil.java
@@ -6,6 +6,27 @@ import java.util.Random;
  * utility functions.
  */
 public final class MathUtil {
+
+  /**
+   * fast inverse square root.
+   * originally from quake 3:
+   * http://en.wikipedia.org/wiki/Fast_inverse_square_root
+   * 
+   * works by making a clever guess as to the starting point
+   * for newton's method. 1 pass is a good approximation.
+   **/
+  public static double invSqrt(double x) {
+    final double xhalf = 0.5d*x;
+    long i = Double.doubleToLongBits(x);
+    i = 0x5fe6eb50c7b537a9L - (i >> 1);
+    x = Double.longBitsToDouble(i);
+    x *= (1.5d - xhalf*x*x); // pass 1
+    x *= (1.5d - xhalf*x*x); // pass 2
+    x *= (1.5d - xhalf*x*x); // pass 3               
+    x *= (1.5d - xhalf*x*x); // pass 4
+    return x;
+  }
+
   //
   // note: sigmoid(double) self time = 42.61%.
   // sigmoid calculation is the main hotspot; this is due to the expensive
